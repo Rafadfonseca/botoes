5973d204ec79ef8235b109754b06a7d954b4019c
rafaela <rafaeladf2007@gmail.com>
2023-09-02 17:48:08 -0300

    pronto

---
+++ script.js
@@ -0,0 +1,40 @@
+// obrigatório
+$(document).ready(function(){
+
+    //oque cada botão faz
+    $("#option1").click(function(){
+        alert("Aprendemos a utilizar o HTML, CSS e o JS no Bootstrap");
+    });
+
+    $("#option2").click(function(){
+        alert("Estamos criando nossos projetos da feira de ciências");
+    });
+    $("#option3").click(function(){
+        alert("Estamos estudando propriedades privadas e como elas foram conseguidas e/ou roubadas");
+    });
+
+    $("#option4").click(function(){
+        alert("Começamos a ver logaritimos");
+    });
+    $("#option5").click(function(){
+        alert("Estamos tendo miniaulas sobre assuntos diversos, eu falei sobre a incentivação de leitura nas escolas");
+    });
+
+    $("#option6").click(function(){
+        alert("Na última aula nós terminamos o assunto entrevista");
+    });
+    $("#option7").click(function(){
+        alert("Vocabulário de técnologia");
+    });
+
+    $("#option8").click(function(){
+        alert("Estamos criando um curta e na última aula o assunto foi edição");
+    });
+    $("#option9").click(function(){
+        alert("Bio:Consumo de energia e sustentabilidade. Quí:Química inorgânica. Fis:Distância, deslocamento e velocidade média");
+    });
+    $("#option0").click(function(){
+        alert("Estamos na matéria de esportes");
+    });
+
+})
